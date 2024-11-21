8080c689d1693c624afd0745c336b857f3405f4f
Fiyaz <fferdouse@binghamton.edu>
2024-11-20 21:08:12 -0500

    Updated models

--- template_final_project-master/src/CompanyInfo.py
+++ template_final_project-master/src/CompanyInfo.py
@@ -2,6 +2,14 @@ import stockquotes
 
 class CompanyInfo:
     def __init__(self, companyName, fileName, tickerSymbol):
+        """
+        Provides info for the company coming from a premade text file
+        Args:
+            companyName (str): company name
+            fileName (str): name of file for the company
+            tickerSymbol (str): ticker symbol for the company
+        """
+        
         self.name = companyName
         self.info = fileName # in reality, this will be reading from a text file and be inputting the information in a standardized format
         companyStock = stockquotes.Stock(tickerSymbol)
--- template_final_project-master/src/Node.py
+++ template_final_project-master/src/Node.py
@@ -2,9 +2,27 @@ import CompanyInfo
 
 class Node:
     def __init__(self, x, y, companyName, companyLogo):
+        """
+        Creates a node at (x, y) with the company logo
+        Args:
+            x (int): x coord
+            y (int): y coord
+            companyName (string): name of company
+            companyLogo (string): logo file name
+        Returns: none
+        """
+        
         self.x = x
         self.y = y
         self.name = companyName
         self.clickable = True
         self.logo = companyLogo # file string
         self.size = 5 # arbitrary number
+        
+    def openWindowOnClick(self):
+        """
+        Opens a window upon the user clicking on the window,
+        will not open more than one window per node at a time
+        args: none
+        returns: none
+        """
