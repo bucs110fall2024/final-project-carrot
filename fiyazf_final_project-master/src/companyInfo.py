64165033e4d2d8d7256472efd2d6964b5f4cc22d
Fiyaz <fferdouse@binghamton.edu>
2024-12-04 21:11:33 -0500

    company information

--- fiyazf_final_project-master/src/companyInfo.py
+++ fiyazf_final_project-master/src/companyInfo.py
@@ -1,7 +1,8 @@
-import stockquotes
+import yfinance as yf
+import json
 
 class CompanyInfo:
-    def __init__(self, companyName, fileName, tickerSymbol):
+    def __init__(self, companyName, tickerSymbol):
         """
         Provides info for the company coming from a premade text file
         Args:
@@ -9,8 +10,19 @@ class CompanyInfo:
             fileName (str): name of file for the company
             tickerSymbol (str): ticker symbol for the company
         """
-        
         self.name = companyName
-        self.info = fileName # in reality, this will be reading from a text file and be inputting the information in a standardized format
-        companyStock = stockquotes.Stock(tickerSymbol)
-        self.stockPrice = companyStock.current_price
+        self.ticker = yf.Ticker(tickerSymbol)
+        self.fileTicker = tickerSymbol
+        with open("assets/companyFolders.json", "r") as file:
+            self.companyFolders = json.load(file)
+        
+    def stockPrice(self):
+        todays_data = self.ticker.history(period='1d')
+        return todays_data['Close'].iloc[0]
+    
+    def retrieveInformation(self, ticker):
+        folderPath = self.companyFolders.get(ticker)
+        filePath = f"{folderPath}/info.txt"
+        print(filePath)
+    
+CompanyInfo('Goldman Sachs', 'GS').retrieveInformation('GS')
